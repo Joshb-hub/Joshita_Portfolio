b7460fef791022b1f7c0182252703373ee4d572e
Joshb-hub <joshbhatta7@gmail.com>
2026-09-06 03:00:27 +0530

    Update portfolio

--- lib/site-profile.ts
+++ lib/site-profile.ts
@@ -9,23 +9,24 @@ export const profile = {
   firstName: 'Joshita',
   lastName: 'Bhattacharyya',
   initials: 'JB',
-  titles: ['System Engineer', 'Frontend Developer', 'Web Developer', 'Data & AI Enthusiast'] as const,
+  titles: ['System Engineer', 'Frontend Developer', 'Web Developer', 'Data & AI Enthusiast', 'Prompt Engineer','Cloud Engineer'] as const,
   location: 'Kolkata, India',
   email: 'joshitabhattacharyya@gmail.com',
   phone: '+91 95505 72523',
   phoneHref: 'tel:+919550572523',
 
   // TODO: replace with your GitHub profile URL
-  github: 'https://github.com/YOUR_GITHUB_USERNAME',
+  github: 'https://github.com/Joshb-hub',
   // TODO: replace with your LinkedIn profile URL
-  linkedin: 'https://www.linkedin.com/in/YOUR_LINKEDIN_SLUG',
+  linkedin: 'https://www.linkedin.com/in/joshita-bhattacharyya-3200a3267/',
   // TODO: replace with your LeetCode profile URL
-  leetcode: 'https://leetcode.com/u/YOUR_LEETCODE_USERNAME',
+  leetcode: 'https://leetcode.com/u/777-7-777j/',
   // TODO: replace with your HackerRank profile URL
-  hackerrank: 'https://www.hackerrank.com/YOUR_HACKERRANK_USERNAME',
+  hackerrank: 'https://www.hackerrank.com/profile/joshitabhattach1',
+  unstop: 'https://unstop.com/u/joshibha5135',
 
   // TODO: add a professional photo at public/profile.jpg
-  profileImage: '/profile.jpg',
+  profileImage: '/profile.png',
   // TODO: add the latest resume PDF at public/resume.pdf
   resume: '/resume.pdf',
 }
